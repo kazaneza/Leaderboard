627f4ce0a3ff1b8c8805a4cc09263d98fed20cc2
kazaneza <kazanezagentil@gmail.com>
2023-02-09 01:14:09 +0100

    add functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+import { addScore, getScore } from './scores.js';
+
+const refreshButton = document.getElementById('refresh-button');
+refreshButton.addEventListener('click', getScore);
+
+const addForm = document.getElementById('add-form');
+addForm.addEventListener('submit', (event) => {
+event.preventDefault();
+
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+
+addScore(nameInput.value, scoreInput.value);
+clearInputs(nameInput, scoreInput);
+});
+
+const clearInputs = (...inputs) => {
+inputs.forEach((input) => (input.value = ''));
+};
+
+
+
+//1xyAF0OXINI4Powl80fQ

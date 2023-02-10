dfb736fec7d7e9cfe64096dedf0821fe99b7d34f
kazaneza <kazanezagentil@gmail.com>
2023-02-10 18:27:01 +0100

    changed the id not working

--- src/display.js
+++ src/display.js
@@ -1,12 +1,14 @@
 const displayScores = (scores) => {
-    const scoresList = document.querySelector('.recent-display');
+    const scoresList = document.getElementById('scores-list');
     scoresList.innerHTML = '';
-    
+    let counter = 1;
+  
     scores.forEach((score) => {
-    const item = document.createElement('li');
-    item.innerHTML = `${score.user}: ${score.score}`;
-    scoresList.appendChild(item);
+      const item = document.createElement('li');
+      item.innerHTML = `${counter}. ${score.user}: ${score.score}`;
+      scoresList.appendChild(item);
+      counter += 1;
     });
-    };
-    
-    export default displayScores;
+  };
+  
+  export default displayScores;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import './style.css';
 import { addScore, getScore } from './scores.js';
 
-const refreshButton = document.getElementById('refresh-button');
-refreshButton.addEventListener('click', getScore);
 
-const addForm = document.getElementById('add-form');
-addForm.addEventListener('submit', (event) => {
-event.preventDefault();
-
-const nameInput = document.getElementById('name');
-const scoreInput = document.getElementById('score');
-
-addScore(nameInput.value, scoreInput.value);
-clearInputs(nameInput, scoreInput);
-});
-
-const clearInputs = (...inputs) => {
-inputs.forEach((input) => (input.value = ''));
-};
-
-
-
-//1xyAF0OXINI4Powl80fQ
+document.getElementById('refresh-button').addEventListener('click', () => {
+    getScore();
+  });
+  
+  document.getElementById('add-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const user = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    addScore(user, score);
+  });
+  
+  function clearInputs() {
+    document.getElementById('name').value = '';
+    document.getElementById('score').value = '';
+  }
+  
+  document.getElementById('add-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const user = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    addScore(user, score);
+    clearInputs();
+  });
